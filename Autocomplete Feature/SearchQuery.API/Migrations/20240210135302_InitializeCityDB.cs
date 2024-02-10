a957d81ad990ab3a834911b4402e9fb090100204
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2024-02-10 19:25:56 +0530

    Add Migration API related dependancies and and create Initial cities database in sqllite.

---
+++ Autocomplete Feature/SearchQuery.API/Migrations/20240210135302_InitializeCityDB.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SearchQuery.API.Migrations
+{
+    public partial class InitializeCityDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CityName = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cities");
+        }
+    }
+}
