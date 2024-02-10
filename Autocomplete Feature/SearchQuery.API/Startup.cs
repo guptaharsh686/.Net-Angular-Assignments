d8d6e4d0f95020ff9a8541c7ac0066f71213038f
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2024-02-10 18:45:01 +0530

    Add appsettings.json file to access appsettings in Startup.cs

--- Autocomplete Feature/SearchQuery.API/Startup.cs
+++ Autocomplete Feature/SearchQuery.API/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,8 +9,15 @@ namespace SearchQuery.API
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            var x = Configuration.GetConnectionString("WebApiDatabase");
             services.AddControllers();
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
