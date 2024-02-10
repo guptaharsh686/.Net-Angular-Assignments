8933c5e887144d34de3ee8c98ebfc5e8619944f4
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2024-02-10 16:05:33 +0530

    Step 3 : Add controller and routing support

---
+++ Autocomplete Feature/SearchQuery.API/Controllers/CitiesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SearchQuery.API.Controllers
+{
+    [ApiController]
+    [Route("test")]
+    public class CitiesController : ControllerBase
+    {
+        public string Get()
+        {
+            return "Returning from Cities Controller";
+        }
+    }
+}
---
+++ Autocomplete Feature/SearchQuery.API/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SearchQuery.API
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
